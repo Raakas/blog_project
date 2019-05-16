9badb1b0fbbd7b796f45bbe78c9284ace8042580
Raakas <arttu.tiiro@gmail.com>
2019-05-16 14:59:26 +0300

    Update comments.js

--- public/comments.js
+++ public/comments.js
@@ -3,5 +3,7 @@ const textarea = document.querySelector('textarea');
 const button = document.querySelector('button');
 const chat = document.querySelector('#chat');
 
+button.addEventListener('click', () => {
     addComment(textarea.value);
+    textarea.value = "";
 });
