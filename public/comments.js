770931082e5d2b266b9f6c90e56089948e427471
Raakas <arttu.tiiro@gmail.com>
2019-05-16 14:59:12 +0300

    GET and POST
    get and post methods
    blog comments are saved on server

--- public/comments.js
+++ public/comments.js
@@ -1,12 +1,7 @@
+
 const textarea = document.querySelector('textarea');
 const button = document.querySelector('button');
 const chat = document.querySelector('#chat');
 
-const addComment = (value) => {
-    var comment = document.createTextNode(value);
-    chat.append(comment);
-}
-
-button.addEventListener('click', () =>{
     addComment(textarea.value);
 });
--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 const PORT = 5000;
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}) );
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
-    response.sendfile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/index.html');
+
 });
 
 app.get('/blog', (request, response) => {
-    response.sendfile(__dirname + '/blog.html');
+
+    fs.readFileSync('chat.json', response, (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
+    
+    response.sendFile(__dirname + '/blog.html');
 });
 
 app.post('/blog', (request, response) => {
-    response.redirect('/blog');
+
+    fs.writeFileSync('chat.json', request.body.comment, {flag: "a"}, (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
+    
+    response.redirect('/');
 });
 
 app.listen(PORT, () => {
