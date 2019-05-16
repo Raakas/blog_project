c0c00c683381cb993a8b6dd5e74cd82a4cdef981
Raakas <arttu.tiiro@gmail.com>
2019-05-16 10:11:09 +0300

    Update comments.js
    testing

--- public/comments.js
+++ public/comments.js
@@ -5,6 +5,7 @@ const chat = document.querySelector('#chat');
 const addComment = (value) => {
     var comment = document.createTextNode(value);
     chat.append(comment);
+    console.log('git test');
 }
 
 button.addEventListener('click', () =>{
