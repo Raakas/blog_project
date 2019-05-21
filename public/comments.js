f176a76e6d2576116dea5ce065a3a7ce67083cea
Raakas <arttu.tiiro@gmail.com>
2019-05-21 15:12:10 +0300

    register
    register username and password to json

--- public/comments.js
+++
@@ -1,9 +0,0 @@
-
-const textarea = document.querySelector('textarea');
-const button = document.querySelector('button');
-const chat = document.querySelector('#chat');
-
-button.addEventListener('click', () => {
-    addComment(textarea.value);
-    textarea.value = "";
-});
--- server.js
+++ server.js
@@ -19,6 +19,19 @@ app.get('/', (request, response) => {
 
 app.get('/blog', (request, response) => {
 
+    const path = 'chat.json'
+
+    try {
+        if (!fs.existsSync(path)) {
+            const json = {"chat":[]};
+            const data = JSON.stringify(json);
+            fs.writeFileSync('chat.json', data);
+        }
+    }
+    catch(err) {
+        console.error(err)
+    }
+
     const data = fs.readFileSync('chat.json');
     const chat = JSON.parse(data);
 
@@ -27,12 +40,46 @@ app.get('/blog', (request, response) => {
 
 app.post('/blog', (request, response) => {
 
-    data = fs.readFileSync('chat.json')
+    data = fs.readFileSync('chat.json');
     let obj = JSON.parse(data);
     obj.chat.push({"name": request.body.name, "comment": request.body.comment});
     const json = JSON.stringify(obj);
 
     fs.writeFileSync('chat.json', json, {flags: 'a'}, (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
+
+    response.redirect('/blog');
+});
+
+app.get('/register', (request, response) => {
+
+    const path = 'register.json'
+
+    try {
+        if (!fs.existsSync(path)) {
+            const json = {"register":[]};
+            const data = JSON.stringify(json);
+            fs.writeFileSync('register.json', data);
+        }
+    }
+    catch(err) {
+        console.error(err)
+    }
+
+    response.render('register.ejs');
+})
+
+app.post('/register', (request, response) => {
+      
+    data = fs.readFileSync('register.json');
+    let obj = JSON.parse(data);
+    obj.register.push({"username": request.body.username, "password": request.body.password});
+    const json = JSON.stringify(obj);
+
+    fs.writeFileSync('register.json', json, (err) => {
         if(err){
             console.log(err);
         }
